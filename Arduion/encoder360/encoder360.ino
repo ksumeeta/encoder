208cde8eaba4c015f378ce6dac48ae640a0c9a8a
ksumeeta <ksumeeta@gmail.com>
2023-05-07 12:58:18 +0530

    Stage 4 - Code Cleaning NOT Tested Code

--- Arduion/encoder360/encoder360.ino
+++ Arduion/encoder360/encoder360.ino
@@ -1,19 +1,23 @@
-/**************
- * VCC to 5V  *
- * GND to GND *
- * CLK to D3  *
- * CLK to D4  *
- **************/
+#define BalDeg1 4
+#define BalDeg2 160
+#define mLeft  0
+#define mRight 1
+#define mSlow  750
+#define mFast  750
+#define MaxMoveLength 1000
+#define ResetMoveLength 500
+#define RailLength  1200
+#define MoveLengthIncrement 10
+
+
+//PIN Defination
 const int pinA = 2;
 const int pinB = 3; 
-
-const int pinLend = 4;
-const int pinRend = 5;
-
 const int stepPin = 6; 
 const int dirPin = 7; 
 const int enPin = 8;
 
+//Encoder Variables
 volatile int lastEncoded = 0;
 volatile int encoderValue = 0;
 int eValue = 0;
@@ -23,20 +27,9 @@ long lastencoderValue = 0;
 int lastMSB = 0;
 int lastLSB = 0;
 
+//Pendulum Variables
 boolean PDir = false;
 boolean SDir = false;
-boolean LeftEnd = false;
-boolean RightEnd = false;
-
-#define BalDeg1 4
-#define BalDeg2 160
-#define mLeft  0
-#define mRight 1
-#define mSlow  750
-#define mFast  750
-#define MaxMoveLength 1000
-#define ResetMoveLength 500
-#define MoveLengthIncrement 10
 
 int MovPos = 0;
 int MovLen = ResetMoveLength;
@@ -46,24 +39,21 @@ int skipCnt = 0;
 void setup(){ 
   Serial.begin (9600);
 
+  //Motor Controller Pins
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(enPin,OUTPUT);
   digitalWrite(enPin,HIGH);
-  
+
+  //Encoder Pins
   pinMode(pinA, INPUT_PULLUP); 
   pinMode(pinB, INPUT_PULLUP);
-  pinMode(pinLend, INPUT); 
-  pinMode(pinRend, INPUT);
 
-  //call updateEncoder() when any high/low changed seen
-  //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
+  //call updateEncoder() when any  Change (high/low) occurs on interrupt 0(pin 2), or interrupt 1(pin 3) 
   attachInterrupt(digitalPinToInterrupt(pinA), updateEncoder, CHANGE); 
   attachInterrupt(digitalPinToInterrupt(pinB), updateEncoder, CHANGE);
  
    Serial.println("BEGIN");
-
-   //ToDo Move to Center
 } 
 
 void updateEncoder(){
@@ -107,21 +97,25 @@ inline static void MoveMotor(int Dir, int Delay){
   }
 }
 
+inline static void MoveToCenter(){
+  if(AbsPos < 0){
+    SDir = true;
+  }else{
+    SDir = false;
+  }
+  while(AbsPos != 0){
+    MoveMotor(SDir, 1000);
+  }
+  MovPos = 0;
+  MovLen = ResetMoveLength;
+}
+
 void loop() { 
   eValue = encoderValue;
   eValueA = abs(eValue);
-  if(abs(AbsPos)>1200){
-    if(AbsPos < 0){
-        SDir = true;
-      }else{
-        SDir = false;
-      }
-      while(AbsPos != 0){
-        MoveMotor(SDir, 1000);
-      }
-      MovPos = 0;
-      MovLen = ResetMoveLength;
-  }else if((eValueA > (720 - BalDeg1)) && (eValueA < (720 + BalDeg1))){       //Pendulum is in Balanced Zone i.e. 1~2 degree variation from upright position. Don't Move Motor
+  if(abs(AbsPos) > RailLength){
+    MoveToCenter();
+  }else if((eValueA > (720 - BalDeg1)) && (eValueA < (720 + BalDeg1))){ //Pendulum is in Balanced Zone i.e. 1~2 degree variation from upright position. Don't Move Motor
     skipCnt = 500;
   }else if((eValueA > (720 - BalDeg2)) && (eValueA < (720 + BalDeg2))){ //Pendulum is in Balancing Zone i.e. 10 degree varation
     if(eValue < -720){
@@ -134,44 +128,22 @@ void loop() {
       MoveMotor(mRight, mFast);
     }
     skipCnt = 500;
-  }else{                        //Pendulum not in Balancing Zone. Swing the pendulum to get in balancing Zone
-    if(MovPos < MovLen){        //Keep moving Motor in present Swing direction
+  }else{                                                              //Pendulum not in Balancing Zone. Swing the pendulum to get in balancing Zone
+    if(MovPos < MovLen){                                              //Keep moving Motor in present Swing direction
       MovPos++;
-    }else if(abs(AbsPos)>1200){
-      Serial.println("EndReached");
-      if(AbsPos < 0){
-          SDir = true;
-        }else{
-          SDir = false;
-        }
-        while(AbsPos != 0){
-          MoveMotor(SDir, 1000);
-        }
-        MovPos = 0;
-        MovLen = ResetMoveLength;
     }else{                      //Change direction and (Reset to High frequency by Resetting MoveLenght OR Decrease the frequency by increasing MoveLenght)
       SDir = !SDir;
-      if(MovPos > MaxMoveLength){                 //Restart to High frequency Movement of pendulum
-        //ToDo Move to Center
-        if(AbsPos < 0){
-          SDir = true;
-        }else{
-          SDir = false;
-        }
-        while(AbsPos != 0){
-          MoveMotor(SDir, 1000);
-        }
-        MovPos = 0;
-        MovLen = ResetMoveLength;
+      if(MovPos > MaxMoveLength){                                     //Restart to High frequency Movement of pendulum
+        MoveToCenter();
       }else{
-//        delay(1);
+        delay(5);
         MovPos = 0;
         MovLen = MovLen + MoveLengthIncrement;   //Decrease the frequency by Increasing the Movement Lenght to get the pendulum upright
       }  
     }
     if(skipCnt == 0){
       MoveMotor(SDir, mSlow);
-    }else{
+    }else{      //Wait for the pendulum to be stable
       skipCnt--;
       delay(1);
     }
